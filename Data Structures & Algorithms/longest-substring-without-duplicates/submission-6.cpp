f4fecd1fc5fd066f0a22abc759993c1fa6735a1d
Khaled Elbalal <KhaledElbalal@gmail.com>
2026-04-24 21:00:55 +0300

    Add: longest-substring-without-duplicates - submission-6

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-6.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    int lengthOfLongestSubstring(string s) {
+        if(s.empty()) return 0;
+        int ans = 1;
+        for(int i = 0; i < s.size(); i++) {
+            unordered_map<char,int> isExist;
+            isExist[s[i]] = i; 
+            bool terminated = false;
+            for(int r = i + 1; r < s.size(); r++) {
+                if(isExist.contains(s[r])) {
+                    i = isExist[s[r]];
+                    ans = max(ans, r - i);
+                    terminated = true;
+                    break;
+                } else {
+                    isExist[s[r]] = i;
+                    ans = max(ans, r - i + 1);
+                }
+            }
+            if(!terminated) break;
+        }
+        return ans;
+    }
+};
