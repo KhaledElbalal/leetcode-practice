7bddf66c116c2700ede2a31b47102d8972c51e83
Khaled Elbalal <KhaledElbalal@gmail.com>
2026-07-13 02:52:11 +0300

    Add: n-th-tribonacci-number - submission-0

---
+++ Data Structures & Algorithms/n-th-tribonacci-number/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    unordered_map<int, int> mp;
+    int tribonacci(int n) {
+        if(n == 0) return 0;
+        if(n == 1 || n == 2) return 1;
+        if(mp.count(n)) return mp[n];
+        else {
+            int result = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
+            mp[n] = result;
+            return result;
+        }
+    }
+};
