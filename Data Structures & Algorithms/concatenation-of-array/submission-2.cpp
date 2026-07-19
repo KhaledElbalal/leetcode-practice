63c6c49442a5380c367749ec99ce83ce0640ed80
Khaled Elbalal <KhaledElbalal@gmail.com>
2026-07-19 21:28:49 +0300

    Add: concatenation-of-array - submission-2

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-2.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> result(nums.size() * 2);
+        for(int i = 0; i < nums.size(); i++) {
+            result[i] = result[nums.size() + i] = nums[i];
+        }
+        return result;
+    }
+};
